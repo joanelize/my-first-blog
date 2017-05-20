7b0e702ad12c573429048a808f578c3fd526e22e
joanelize <joanelize.ab@gmail.com>
2017-05-20 14:55:17 -0300

    'jo'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'boa-)%a&+h_1(=$7^d2690^=v4m&(n=)um8nydqljhpl=)yf)i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joanelize.pythonanywhere.com']
 
 
 # Application definition
